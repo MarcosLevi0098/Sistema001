218161c3a9d8c107050047c8243217781a11555f
Marcoslevi <65694101+Marcoslevit007@users.noreply.github.com>
2024-07-08 21:26:37 -0300

    Alguma coisa

--- database/factories/ProdutoFactory.php
+++ database/factories/ProdutoFactory.php
@@ -3,7 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Categoria;
+use App\Models\User;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
@@ -16,8 +17,15 @@ class ProdutoFactory extends Factory
      */
     public function definition(): array
     {
+        $nome = $this->faker->unique()->sentence();
         return [
-            //
+            'nome' => $nome,
+            'descricao' => $this->faker->paragraph(),
+            'preco' => $this->faker->randomNumber(2),
+            'slug' => Str::slug($nome),
+            'image' => $this->faker->imageUrl(400, 400),
+            'id_user'=> User::pluck('id')->ramdom(),
+            'id_categoria' => Categoria::plick('id')->ramdom()
         ];
     }
 }
